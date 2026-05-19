31fd4d0d34fb917f64d901bee1cefe6fd5137e97
jennyznli <jennyznli@gmail.com>
2026-05-19 11:36:32 -0400

    tgct

--- tgct/scripts/archive/select_tgct2.R
+++ tgct/scripts/archive/select_tgct2.R
@@ -18,22 +18,3 @@ tgct_results <- select_cancer_cases(
     data_dir = here("tgct", "data"),
     log_dir = here("tgct", "log")
 )
-
-# # simplexo freeze 3.0 case selection
-# breast_results <- select_cancer_cases(
-#     cancer_name = "breast_simplexo3_v2",
-#     icd_codes = c("^C50", "^Z85.4", "^174", "^V10.3", "^D05", "^Z86.000", "^233.0"),
-#     gender_filter = "Female",
-#     crep_filter = NULL,
-#     min_instances = 2,
-#     min_timespan = NULL,
-#     age_filter = NULL,
-#     exclude = FALSE,
-#     exclude_codes = c("^C(?!44)", "^Z85(?!\\.828)", "^(?!173)(?:1[4-9][0-9]|20[0-8]|209\\.[0-3])", "^V10(?!\\.83)"),
-#     min_exclude_instances = 0,
-#     min_exclude_timespan = NULL,
-#     output_prefix = NULL,
-#     pmbb_dir = here("PMBB"),
-#     data_dir = here("simplexo", "data"),
-#     log_dir = here("simplexo", "log")
-# )
